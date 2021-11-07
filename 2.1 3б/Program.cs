2e0cd0f027d94a37c6a40c6cdd5a317de3daa168
Teya <Teya@TVOROG>
2021-11-07 19:48:25 +0200

    Добавьте файлы проекта.

---
+++ 2.1 3б/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2._1_3б
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A;
+            Console.WriteLine("Введите число X");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            double y = double.Parse(Console.ReadLine());
+            bool v = x >= 2;
+            bool v1 = Math.Pow(y, 2) != 5;
+            bool v2 = v != v1;
+            //(x >= 2) != (Math.Pow(y, 2) != 5);
+            Console.WriteLine("Ответ:" + v2);
+
+
+        }
+    }
+}
